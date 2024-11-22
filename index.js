3e1fe2cbcfd43cf2fe7266c4907b754012c75c93
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-11-23 03:25:40 +0530

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Docker!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
