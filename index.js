ae8c9d8c644db9283a3ec0cc56586a237030fe3d
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-08-08 13:08:14 +0530

    index file done

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const app = express();
+const path = require("path");
+const posts = require("./routes/posts");
+const users = require("./routes/user");
 
-const PORT = process.env.PORT || 8000;
+const app = express();
+app.use(express.json());
+
+// Import Bootstrap CSS
+app.use(
+	"/css",
+	express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/posts", posts);
+app.use("/users", users);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Docker!");
+	res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
 });
